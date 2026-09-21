b2560b612cdf4980d689627b31d20a67947cd270
aaronzeng-cmu <103963117+aaronzeng-cmu@users.noreply.github.com>
2026-09-21 22:24:57 +0000

    Deploying to gh-pages from @ aaronzeng-cmu/aaronzeng-cmu.github.io@f722300de7089406725d70cc24a53915706e48e5 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -18,17 +18,10 @@ ninja.data = [{
           },
         },{id: "nav-projects",
           title: "projects",
-          description: "A growing collection of your cool projects.",
+          description: "Work in progress.",
           section: "Navigation",
           handler: () => {
             window.location.href = "/projects/";
-          },
-        },{id: "nav-repositories",
-          title: "repositories",
-          description: "Edit the `_data/repositories.yml` and change the `github_users` and `github_repos` lists to include your own GitHub profile and repositories.",
-          section: "Navigation",
-          handler: () => {
-            window.location.href = "/repositories/";
           },
         },{id: "nav-cv",
           title: "cv",
